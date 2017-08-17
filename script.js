7afa957d4d13486e64cb27b4f0b2f805e2fcb262
zelda57 <github@4ic.co.uk>
2017-08-17 14:10:43 +0100

    edited

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 console.log("Addon");
-document.body.style.border = "5px solid blue";
+
+document.body.style.border = "1px solid blue";
 document.title += "poo";
+
+function test() {
+    var elements = document.body.getElementsByTagName("cite");
+    
+    for (var i = 0; i<elements.length; i++) {
+        var element = elements[i];
+        
+        if (element.tagName!=="A") {
+            element.innerHTML.replace(/4ni/g,"xxxx");
+            console.log(element.innerHTML);
+            }
+    }
+}
+
+test();
